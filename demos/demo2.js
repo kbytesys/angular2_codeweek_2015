ee968eafee49e0c5b7566cb0f4f2e16dce0524f2
Andrea Briganti <kbytesys@gmail.com>
2015-10-03 02:25:08 +0200

    base DI

---
+++ demos/demo2.js
@@ -0,0 +1,83 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+// Typescript
+var angular2_1 = require('angular2/angular2');
+var http_1 = require('angular2/http');
+var LivescoreService = (function () {
+    function LivescoreService(http) {
+        this.name = 'Pascal';
+        this.http = http;
+    }
+    LivescoreService.prototype.getName = function () {
+        return this.name;
+    };
+    LivescoreService = __decorate([
+        angular2_1.Injectable(), 
+        __metadata('design:paramtypes', [http_1.Http])
+    ], LivescoreService);
+    return LivescoreService;
+})();
+var LivescoreSummaryComponent = (function () {
+    function LivescoreSummaryComponent(livescoreService) {
+        this.name = livescoreService.getName();
+    }
+    LivescoreSummaryComponent = __decorate([
+        angular2_1.Component({
+            selector: 'livescore-summary',
+            properties: ['name']
+        }),
+        angular2_1.View({
+            template: '<h1>{{ name }}</h1>'
+        }), 
+        __metadata('design:paramtypes', [LivescoreService])
+    ], LivescoreSummaryComponent);
+    return LivescoreSummaryComponent;
+})();
+var LivescoreSearchComponent = (function () {
+    function LivescoreSearchComponent(livescoreService) {
+        this.name = livescoreService.getName();
+    }
+    LivescoreSearchComponent = __decorate([
+        angular2_1.Component({
+            selector: 'livescore-search',
+            properties: ['name']
+        }),
+        angular2_1.View({
+            template: '<h1>Suino: {{ name }}</h1>'
+        }), 
+        __metadata('design:paramtypes', [LivescoreService])
+    ], LivescoreSearchComponent);
+    return LivescoreSearchComponent;
+})();
+var DemoApp2Compnent = (function () {
+    function DemoApp2Compnent() {
+    }
+    DemoApp2Compnent = __decorate([
+        angular2_1.Component({
+            selector: 'demo2'
+        }),
+        angular2_1.View({
+            template: "\n    <div class=\"row\">\n        <div class=\"col-md-6\">\n            <livescore-summary></livescore-summary>\n        </div>\n        <div class=\"col-md-6\">\n            <livescore-search></livescore-search>\n        </div>\n    </div>\n    ",
+            directives: [
+                LivescoreSummaryComponent,
+                LivescoreSearchComponent
+            ]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], DemoApp2Compnent);
+    return DemoApp2Compnent;
+})();
+angular2_1.bootstrap(DemoApp2Compnent, [
+    http_1.HTTP_BINDINGS,
+    LivescoreService
+]);
+//# sourceMappingURL=demo2.js.map
---
+++ demos/demo2.ts
@@ -0,0 +1,75 @@
+// Typescript
+import {Component, Inject, Injectable, View, bootstrap} from 'angular2/angular2';
+import {Http, HTTP_BINDINGS} from 'angular2/http';
+
+@Injectable()
+class LivescoreService {
+    name: string;
+    http: Http;
+    constructor(http: Http) {
+        this.name = 'Pascal';
+        this.http = http;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+@Component({
+    selector: 'livescore-summary',
+    properties: ['name']
+})
+@View({
+    template: '<h1>{{ name }}</h1>'
+})
+class LivescoreSummaryComponent {
+    name: string;
+    constructor(livescoreService: LivescoreService) {
+        this.name = livescoreService.getName()
+    }
+}
+
+@Component({
+    selector: 'livescore-search',
+    properties: ['name']
+})
+@View({
+    template: '<h1>Suino: {{ name }}</h1>'
+})
+class LivescoreSearchComponent {
+    name: string;
+    constructor(livescoreService: LivescoreService) {
+        this.name = livescoreService.getName()
+    }
+}
+
+@Component({
+    selector: 'demo2'
+})
+@View({
+    template: `
+    <div class="row">
+        <div class="col-md-6">
+            <livescore-summary></livescore-summary>
+        </div>
+        <div class="col-md-6">
+            <livescore-search></livescore-search>
+        </div>
+    </div>
+    `,
+    directives: [
+        LivescoreSummaryComponent,
+        LivescoreSearchComponent
+    ]
+})
+class DemoApp2Compnent {
+}
+
+bootstrap(
+    DemoApp2Compnent,
+    [
+        HTTP_BINDINGS,
+        LivescoreService
+    ]
+);
